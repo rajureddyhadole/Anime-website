91885b45d9d1e83c98a22386d4897e5e372148b0
Raju <rajureddyhadole452@gmail.com>
2024-05-21 18:15:44 +0530

    made a change

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,7 @@ export const Search = () => {
     <img className='w-[92%] h-60 mx-auto object-cover' src="./images/goku.jpg" alt="itachi" />
 			<div className='flex items-center mx-auto w-[50%] h-16 border bg-white rounded-md mb-10 shadow-md'>
 				<img className='w-12 h-12 pl-4' src="./magnifying-glass-solid.svg" alt="" />
-				<input className='w-[80%] h-12 border ml-4 pl-4 font-semibold border-none focus:outline-none ' type="text" placeholder='Search'/>
+				<input className='w-[80%] h-12 border ml-4 pl-4 font-semibold border-none focus:outline-none' type="text" placeholder='Search'/>
 				<button className='px-4 py-2 mr-4 text-lg font-medium text-white bg-green-400 border-none rounded-md shadow-md hover:bg-green-500 active:bg-green-600' >Filter</button>
 			</div>
     </>
